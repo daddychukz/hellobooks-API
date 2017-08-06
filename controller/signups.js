e3ad7029ecfd577a2988c1c3456def983c5cc11f
chuks <durugo_chuks@yahoo.com>
2017-08-06 17:34:30 +0100

    create signin API

--- controller/signups.js
+++ controller/signups.js
@@ -16,6 +16,31 @@ const register = (req, res) => {
         .catch(err => res.status(400).send(err));
 };
 
+const login = (req, res) => {
+    return Signup
+    .findOne({
+        where: {
+          email: req.body.email,
+          password: req.body.password
+        },
+      })
+      .then((found) => {
+
+        console.log(found);
+        if (found === null) {
+          return res.status(404).send({
+            message: 'This record does not exists!'
+          });
+        }
+        if (found) {
+          return res.status(200).send({
+            message: 'Welcome to Hello-Books Library Management System',
+          });
+        }
+      });
+  }
+
 module.exports = {
-    register
+    register,
+    login
 }
