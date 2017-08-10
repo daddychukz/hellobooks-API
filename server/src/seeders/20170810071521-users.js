84c2ebea7d2ec0b72db491cdea81a5ff6ba973ca
chuks <durugo_chuks@yahoo.com>
2017-08-10 15:15:44 +0100

    create seed for dummy admin signup values

---
+++ server/src/seeders/20170810071521-users.js
@@ -0,0 +1,26 @@
+
+module.exports = {
+  up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Users', [{
+      fullName: process.env.fullName,
+      email: process.env.fullName,
+      sex: process.env.fullName,
+      userName: process.env.fullName,
+      phoneNumber: process.env.phoneNumber,
+      pasword: process.env.pasword,
+      memLevel: process.env.memLevel,
+      isAdmin: process.env.isAdmin,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], { returning: true });
+  },
+
+  down(queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+    */
+    return queryInterface
+      .bulkDelete('Users', null);
+  }
+};
